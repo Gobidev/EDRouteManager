06e212fa165257d90ce9f0a533e1362959ffaa2f
Gobi 007 <adrian.groh@t-online.de>
2019-05-13 19:35:03 +0200

    added ok button functionality

--- views/inputview.py
+++ views/inputview.py
@@ -1,11 +1,17 @@
 import tkinter as tk
+from tkinter import messagebox
+from EDRouteManager.utils.edsm import is_known
 
 HEIGHT = 170
 WIDTH = 250
 
 
 def ok_button_press():
-    pass
+    commander_name = name_entry.get()
+    if not is_known(commander_name):
+        messagebox.showerror("Error", "Commander is not known to EDSM")
+    else:
+        pass
 
 
 root = tk.Tk()
@@ -24,5 +30,6 @@ name_entry.place(relwidth=0.45, relheight=0.18, relx=0.26, rely=0)
 
 ok_button = tk.Button(frame, text="Ok", command=ok_button_press)
 ok_button.place(relwidth=0.2, relheight=0.18, relx=0.72, rely=0)
+
 
 root.mainloop()
