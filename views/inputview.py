719946bfa65459895175225375e2cb66b94d8109
Gobi 007 <adrian.groh@t-online.de>
2019-05-13 19:24:06 +0200

    added name_label and ok_button to inputview

--- views/inputview.py
+++ views/inputview.py
@@ -3,6 +3,11 @@ import tkinter as tk
 HEIGHT = 170
 WIDTH = 250
 
+
+def ok_button_press():
+    pass
+
+
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
@@ -12,6 +17,12 @@ frame = tk.Frame(root, bd=5)
 frame.place(relwidth=1, relheight=1)
 
 label = tk.Label(frame, text="Name:")
-label.pack()
+label.place(relwidth=0.25, relheight=0.18, relx=0, rely=0)
+
+name_entry = tk.Entry(frame)
+name_entry.place(relwidth=0.45, relheight=0.18, relx=0.26, rely=0)
+
+ok_button = tk.Button(frame, text="Ok", command=ok_button_press)
+ok_button.place(relwidth=0.2, relheight=0.18, relx=0.72, rely=0)
 
 root.mainloop()
