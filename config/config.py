42654c2685002e35b74c8da8662b99acd8b08d3b
Gobidev <adrian.groh@t-online.de>
2019-05-14 16:19:51 +0200

    fixed spelling mistake & added debug messages

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
 import json
 
 
-def set_coommander_name(commander_name):
+def set_commander_name(commander_name):
     config = {"commander_name": commander_name}
     with open("config.json", "w") as f:
         json.dump(config, f)
+        print("set commander_name to", commander_name)
 
 
 def get_commander_name():
--- views/inputview.py
+++ views/inputview.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from EDRouteManager.utils.edsm import is_known
+from EDRouteManager.config.config import *
 import json
 
 HEIGHT = 170
@@ -12,9 +13,7 @@ def ok_button_press():
     if not is_known(commander_name):
         messagebox.showerror("Error", "Commander is not known to EDSM")
     else:
-        config = {"commander_name" : commander_name}
-        with open("EDRouteManager/config/config.json", "w") as f:   # TODO path does not work
-            json.dump(config, f)
+        set_commander_name(commander_name)
         root.destroy()
 
 
