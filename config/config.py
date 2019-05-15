f8179ca5136f7faee58ec56975fe7dd393073730
Gobidev <adrian.groh@t-online.de>
2019-05-15 18:32:09 +0200

    added function to write current system to config

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import json
-
+from utils.edsm import get_commander_system
 
 def set_commander_name(commander_name):
     config = {"commander_name": commander_name}
@@ -25,3 +25,19 @@ def get_file_path():
     config = "file_path"
     with open("config.json", "r") as f:
         return json.load(f)[config]
+
+
+def refresh_current_system():
+    # get commander_name
+    try:
+        commander_name = get_commander_name()
+    except:
+        print("commander name not set")
+        return
+    system = get_commander_system(commander_name)
+    # set system to config
+    config = {"current_system": system}
+    with open("config.json", "w") as f:
+        json.dump(config, f)
+        print("set current_system to", system)
+
