a391f245e42b9148fed68be7e3329e9f42ad4317
Gobidev <adrian.groh@t-online.de>
2019-05-15 19:23:28 +0200

    fixed bug of dublicate config.json in views folder

--- config/config.py
+++ config/config.py
@@ -1,30 +1,31 @@
 import json
 from utils.edsm import get_commander_system
 
+config_filepath = "..\config\config.json"
 
 def set_commander_name(commander_name):
     config = {"commander_name": commander_name}
-    with open("config.json", "w") as f:
+    with open(config_filepath, "w") as f:
         json.dump(config, f)
         print("set commander_name to", commander_name)
 
 
 def get_commander_name():
     config = "commander_name"
-    with open("config.json", "r") as f:
+    with open(config_filepath, "r") as f:
         return json.load(f)[config]
 
 
 def set_file_path(file_path):
     config = {"file_path": file_path}
-    with open("config.json", "w") as f:
+    with open(config_filepath, "w") as f:
         json.dump(config, f)
         print("set file_path to", file_path)
 
 
 def get_file_path():
     config = "file_path"
-    with open("config.json", "r") as f:
+    with open(config_filepath, "r") as f:
         return json.load(f)[config]
 
 
@@ -38,6 +39,6 @@ def refresh_current_system():
     system = get_commander_system(commander_name)
     # set system to config
     config = {"current_system": system}
-    with open("config.json", "w") as f:
+    with open(config_filepath, "w") as f:
         json.dump(config, f)
         print("set current_system to", system)
