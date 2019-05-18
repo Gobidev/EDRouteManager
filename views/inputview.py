f9f1a7978f78fb3b0f359de38e329671afa884f9
Gobi 007 <adrian.groh@t-online.de>
2019-05-18 17:22:37 +0200

    Started implementing API-key input

--- views/inputview.py
+++ views/inputview.py
@@ -26,8 +26,11 @@ def start_input_window(height=170, width=250):
     frame = tk.Frame(root, bd=5)
     frame.place(relwidth=1, relheight=1)
 
-    label = tk.Label(frame, text="Name:")
-    label.place(relwidth=0.25, relheight=0.18, relx=0, rely=0)
+    name_label = tk.Label(frame, text="Name:")
+    name_label.place(relwidth=0.25, relheight=0.18, relx=0, rely=0)
+
+    # api_label = tk.Label(frame, text="API-key (optional):")
+    # api_label.place(relwidth=0.4, relheight=0.18, relx=0, rely=0.19)
 
     name_entry = tk.Entry(frame)
     name_entry.place(relwidth=0.45, relheight=0.18, relx=0.26, rely=0)
