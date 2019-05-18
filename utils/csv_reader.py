af9c7355e7cdad9ba49f402dc79b225d4ef7beb4
Gobidev <adrian.groh@t-online.de>
2019-05-18 11:35:09 +0200

    Catching errors when opening a file

--- utils/csv_reader.py
+++ utils/csv_reader.py
@@ -23,30 +23,29 @@ def csv_to_list(filename):
 
 
 def delete_first_row(table):
-    del table[0]
+    try:
+        del table[0]
+    except:
+        pass
     return table
 
 
 def get_systems(table):
 
     systems = []
-
-    for row in table:
-        systems.append(row[0])
-
+    try:
+        for row in table:
+            systems.append(row[0])
+    except:
+        pass
     return systems
 
 
 def is_system_of_route(commander_system, route_systems):
-
-    for route_system in route_systems:
-        if route_system == commander_system:
-            return True, route_systems.index(route_system)
-    return False, False
-
-
-# ------- only for testing purposes -------
-# table = csv_to_list("example.csv")
-# table = delete_first_row(table)
-# systems = get_systems(table)
-# print(is_system_of_route("Omega Sector VE-Q b5-15", systems))
+    try:
+        for route_system in route_systems:
+            if route_system == commander_system:
+                return True, route_systems.index(route_system)
+        return False, False
+    except:
+        return False, False
