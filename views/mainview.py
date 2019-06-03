73a7dac1c19ee34d5e2f24f093456346cb1fa91c
RubixDev <silas.groh@t-online.de>
2019-06-03 19:56:53 +0200

    Redesigned the window styles

--- views/mainview.py
+++ views/mainview.py
@@ -2,13 +2,14 @@ import tkinter as tk
 from tkinter import filedialog
 from config.config import *
 import threading
+from tkinter import ttk
 
 
 def set_info_content(commander_name, current_system_name, next_system_name):
     info_label_2.config(text=(commander_name + "\n" + current_system_name + "\n" + next_system_name))
 
 
-def start_main_window(height=100, width=425):
+def start_main_window(height=80, width=425):
     global info_label_2
 
     def open_button_press():
@@ -29,26 +30,27 @@ def start_main_window(height=100, width=425):
 
     root = tk.Tk()
 
-    root.title("EDRM")
-    root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='../views/logo.gif'))
+    root.title("EDRouteManager")
+    root.tk.call('wm', 'iconphoto', root.w, tk.PhotoImage(file='../views/logo.gif'))
     root.iconbitmap(default='../views/logo.gif')
+    root.resizable(False, False)
 
     canvas = tk.Canvas(root, height=height, width=width)
     canvas.pack()
 
-    frame = tk.Frame(root, bd=5)
+    frame = tk.Frame(root, bd=15)
     frame.place(relwidth=1, relheight=1)
 
-    open_button = tk.Button(frame, text="Open Route", command=open_button_press)
-    open_button.place(relx=1-1/5, rely=0, relwidth=1/5, relheight=1/3)
+    open_button = ttk.Button(frame, text="Open Route", command=open_button_press)
+    open_button.grid(row=0, column=2, padx=10, ipadx=20, sticky="E")
 
-    settings_button = tk.Button(frame, text="Settings", command=settings_button_press)
-    settings_button.place(relx=1-1/5, rely=0.3+0.05, relwidth=1/5, relheight=1/3)
+    settings_button = ttk.Button(frame, text="Settings", command=settings_button_press)
+    settings_button.grid(row=1, column=2, padx=10, ipadx=20, sticky="E")
 
-    info_label = tk.Label(frame, text="Commander Name:\nCurrent System:\nNext System:", justify="left")
-    info_label.place(relx=0.03, rely=0.05, relwidth=1/4, relheight=0.6)
+    info_label = ttk.Label(frame, text="Commander Name:\nCurrent System:\nNext System:", justify="left")
+    info_label.grid(row=0, column=0, padx=5, rowspan=3, sticky="W")
 
-    info_label_2 = tk.Label(frame, text="\n \n \n", justify="left")
-    info_label_2.place(relx=0.35, rely=0.05, relwidth=1/3, relheight=0.6)
+    info_label_2 = ttk.Label(frame, text="\n \n \n", justify="left")
+    info_label_2.grid(row=0, column=1, padx=10, rowspan=3, sticky="W")
 
     root.mainloop()
